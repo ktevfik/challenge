030719dec4d23e8c48e6fb887f818a59e77f274d
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-11-21 17:09:36 +0300

    day 4 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,11 @@
 class Solution {
-    public String toLowerCase(String s) {
+    public String replaceDigits(String s) {
+        // even letter - digit odd
         StringBuilder sf = new StringBuilder();
 
-        for (int i = 0;i < s.length();i++) {
-            if (Character.isUpperCase(s.charAt(i))) {
-                sf.append((char)(s.charAt(i) + 32));
+        for (int i = 0; i < s.length();i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                sf.append((char) (s.charAt(i - 1) + Character.getNumericValue(s.charAt(i))));
             } else {
                 sf.append(s.charAt(i));
             }
