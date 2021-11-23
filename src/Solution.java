ee2e8920c7449e47273fcd070bb06fdbb882a8ba
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-11-23 21:36:52 +0300

    day 6 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,9 @@
 class Solution {
-    public int diagonalSum(int[][] mat) {
-        int sum = 0;
-        for (int i = 0;i < mat.length;i++) {
-            for (int j = 0;j < mat[i].length;j++) {
-                if (i == j) {
-                    sum += mat[i][j];
-                    System.out.println(mat[i][j]);
-                }
-
-                if (i + j == mat.length) {
-                    sum += mat[i][j];
-
-                    System.out.println(mat[i][j]);
-
-                }
-            }
+    public void reverseString(char[] s) {
+        for (int i = 0, j = s.length - 1;i < j;i++,j--) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
         }
-        System.out.println(sum);
-        int minusVal = (mat[0].length % 2 == 1 ? mat[mat.length / 2][mat[0].length / 2] : 0);
-        return sum - minusVal;
     }
 }
