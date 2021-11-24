a9bf16dfeaafee1ea703b657a64c6b450ceabc7c
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-11-24 23:38:08 +0300

    day 7 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,19 @@
 class Solution {
-    public void reverseString(char[] s) {
-        for (int i = 0, j = s.length - 1;i < j;i++,j--) {
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
+   public int countConsistentStrings(String allowed, String[] words) {
+        int ans=words.length;
+        Set<Character> hash_set = new HashSet<>();
+        for(int k=0;k<allowed.length();k++){
+            hash_set.add(allowed.charAt(k));
         }
-    }
+        for(String s : words){
+            for(int i=0;i<s.length();i++){
+                if(!hash_set.contains(s.charAt(i))){
+                    ans--;
+                    break;
+                }
+            }
+        }
+        return ans;
+    } 
 }
+
