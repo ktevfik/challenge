7d650b056c497d81385ae882548a0d24217daabc
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-11-25 19:58:05 +0300

    day 8 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,9 @@
 class Solution {
-   public int countConsistentStrings(String allowed, String[] words) {
-        int ans=words.length;
-        Set<Character> hash_set = new HashSet<>();
-        for(int k=0;k<allowed.length();k++){
-            hash_set.add(allowed.charAt(k));
+    public int xorOperation(int n, int start) {
+        int result = start;
+        for (int i = 1;i < n;i++) {
+            result ^= (start + 2 * i);
         }
-        for(String s : words){
-            for(int i=0;i<s.length();i++){
-                if(!hash_set.contains(s.charAt(i))){
-                    ans--;
-                    break;
-                }
-            }
-        }
-        return ans;
-    } 
+        return result;
+    }
 }
-
