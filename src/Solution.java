66b7911d2ae8c1075333752e35b842217ea06809
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-11-27 15:59:47 +0300

    day 10 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,17 @@
-import java.util.Arrays;
-
 class Solution {
-    public int minMovesToSeat(int[] seats, int[] students) {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
         int counter = 0;
-        Arrays.sort(seats);
-        Arrays.sort(students);
 
-        for (int i = 0;i < students.length;i++) {
-            counter += Math.abs(students[i] - seats[i]);
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (Math.abs(arr[i] - arr[j]) <= a) {
+                    for (int k = j + 1; k < arr.length; k++) {
+                        if (Math.abs(arr[j] - arr[k]) <= b) {
+                            if (Math.abs(arr[i] - arr[k]) <= c) counter++;
+                        }
+                    }
+                }
+            }
         }
 
         return counter;
