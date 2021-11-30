0c4c5a38073319cef449079b7dcaf92416ceed6b
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-11-30 23:33:50 +0300

    day 13 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,35 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
-    public List<Integer> targetIndices(int[] nums, int target) {
-        Arrays.sort(nums);
+    public boolean halvesAreAlike(String s) {
+        char[] arr = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
 
-        List<Integer> list = new ArrayList<>();
+        StringBuilder sf1 = new StringBuilder();
+        StringBuilder sf2 = new StringBuilder();
 
-        for (int i = 0;i < nums.length;i++) {
-            if (nums[i] == target) {
-                list.add(i);
+        for (int i = 0; i < s.length() / 2; i++) {
+            sf1.append(s.charAt(i));
+        }
+        for (int i = s.length() / 2; i < s.length(); i++) {
+            sf2.append(s.charAt(i));
+        }
+
+
+        int cVowel1 = 0;
+        int cVowel2 = 0;
+
+        String s1 = sf1.toString();
+        String s2 = sf2.toString();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < s1.length(); j++) {
+                if (arr[i] == s1.charAt(j)) cVowel1++;
             }
         }
-        return list;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < s2.length(); j++) {
+                if (arr[i] == s2.charAt(j)) cVowel2++;
+            }
+        }
+
+        return cVowel1 == cVowel2;
     }
 }
