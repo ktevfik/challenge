09badb5ca807f0cce2c577e6002e94319150555c
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-12-01 23:42:41 +0300

    day 14 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,14 @@
 class Solution {
-    public boolean halvesAreAlike(String s) {
-        char[] arr = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    public int minOperations(int[] nums) {
 
-        StringBuilder sf1 = new StringBuilder();
-        StringBuilder sf2 = new StringBuilder();
-
-        for (int i = 0; i < s.length() / 2; i++) {
-            sf1.append(s.charAt(i));
-        }
-        for (int i = s.length() / 2; i < s.length(); i++) {
-            sf2.append(s.charAt(i));
-        }
-
-
-        int cVowel1 = 0;
-        int cVowel2 = 0;
-
-        String s1 = sf1.toString();
-        String s2 = sf2.toString();
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < s1.length(); j++) {
-                if (arr[i] == s1.charAt(j)) cVowel1++;
-            }
-        }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < s2.length(); j++) {
-                if (arr[i] == s2.charAt(j)) cVowel2++;
+        int result = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                result += nums[i - 1] + 1 - nums[i];
+                nums[i] = nums[i - 1] + 1;
             }
         }
 
-        return cVowel1 == cVowel2;
+        return result;
     }
 }
