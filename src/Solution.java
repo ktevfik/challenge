b16f6e7f5e030d62f672109e9c0cd5385bfea472
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-12-07 22:39:13 +0300

    day 20 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 class Solution {
-    public int numOfStrings(String[] patterns, String word) {
-        int count = 0;
-        for (int i = 0; i < patterns.length; i++) {
-            if (word.contains(patterns[i])) {
-                count++;
-            }
+    public int[] countPoints(int[][] points, int[][] queries) {
+        // plance coordinates - points
+        // circle x,y, Radius - queries
+
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int[] q : queries) {
+            int cnt = 0, rr = q[2] * q[2];
+            for (int[] p : points)
+            cnt += ((q[0] - p[0]) * (q[0] - p[0]) + (q[1] - p[1]) * (q[1] - p[1]) <= rr) ? 1 : 0;
+            res.add(cnt);
         }
-        return count;
+
+        return res.stream().mapToInt(i -> i).toArray();
     }
+
+
 }
