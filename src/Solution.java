2bcf9a54795ba5449a0c3402324f231d2f45f478
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-12-09 00:02:10 +0300

    day 21 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,19 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 class Solution {
-    public int[] countPoints(int[][] points, int[][] queries) {
-        // plance coordinates - points
-        // circle x,y, Radius - queries
-
-        ArrayList<Integer> res = new ArrayList<>();
-        for (int[] q : queries) {
-            int cnt = 0, rr = q[2] * q[2];
-            for (int[] p : points)
-            cnt += ((q[0] - p[0]) * (q[0] - p[0]) + (q[1] - p[1]) * (q[1] - p[1]) <= rr) ? 1 : 0;
-            res.add(cnt);
-        }
-
-        return res.stream().mapToInt(i -> i).toArray();
+    public int oddCells(int n, int m, int[][] indices) {
+        int count = 0;
+        int row[] = new int [n];
+        int col[] = new int [m];
+        for(int x[] : indices)
+        {
+            row[x[0]]++;
+            col[x[1]]++;
+        }    
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+            {
+                if((row[i]+col[j])%2!=0)
+                    count++;
+            }        
+        return count;
     }
-
-
 }
