3590b6df1080d3e61065c54b8842dc9515dcdbac
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-12-13 17:29:23 +0300

    day 25 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,11 @@
 class Solution {
-    public int reverse(int x) {
-        StringBuilder sf = new StringBuilder();
-        sf.append(x);
-
-        for (int i = 0,j = sf.length() - 1;i < j;i++,j--) {
-            char temp = sf.charAt(i);
-            sf.setCharAt(i, sf.charAt(j));
-            sf.setCharAt(j, temp);
+    public double myPow(double x, int n) {
+        if(n == 0)
+            return 1;
+        if(n<0){
+            n = -n;
+            x = 1/x;
         }
-        if (sf.charAt(sf.length() - 1) == '-') {
-            sf.deleteCharAt(sf.length() -1);
-            sf.insert(0,"-");
-        }
-        int res = 0;
-        try {
-            res = Integer.parseInt(sf.toString());
-        } catch (NumberFormatException err) {
-            res = 0;
-        }
-        return res;
+        return (n%2 == 0) ? pow(x*x, n/2) : x*pow(x*x, n/2);
     }
 }
