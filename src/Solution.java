3b1651be4b0e1f19a2add2285678d07663f40a51
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-12-17 18:42:22 +0300

    day 29 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,27 @@
 class Solution {
-    public double myPow(double x, int n) {
-        if(n == 0)
-            return 1;
-        if(n<0){
-            n = -n;
-            x = 1/x;
+    public int countPoints(String rings) {
+        int[][] arr = new int[201][3];
+        int result = 0;
+        for (int i = 0;i < rings.length();i += 2) {
+            if (rings.charAt(i) == 'R') {
+                arr[charToInt(rings.charAt(i + 1))][0]++;
+            } else if (rings.charAt(i) == 'G') {
+                arr[charToInt(rings.charAt(i + 1))][1]++;
+            } else if (rings.charAt(i) == 'B') {
+                arr[charToInt(rings.charAt(i + 1))][2]++;
+            }
         }
-        return (n%2 == 0) ? pow(x*x, n/2) : x*pow(x*x, n/2);
+
+        for (int i = 0;i < arr.length;i++) {
+            if (arr[i][0] > 0 && arr[i][1] > 0 && arr[i][2] > 0) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public int charToInt(char a) {
+        return Integer.parseInt(String.valueOf(a));
     }
 }
