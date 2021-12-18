0a935e1a63fe00689f50f869ff951a10c44bfa57
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-12-18 18:28:42 +0300

    day 30 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,28 @@
+import java.util.Arrays;
+
 class Solution {
-    public int countPoints(String rings) {
-        int[][] arr = new int[201][3];
-        int result = 0;
-        for (int i = 0;i < rings.length();i += 2) {
-            if (rings.charAt(i) == 'R') {
-                arr[charToInt(rings.charAt(i + 1))][0]++;
-            } else if (rings.charAt(i) == 'G') {
-                arr[charToInt(rings.charAt(i + 1))][1]++;
-            } else if (rings.charAt(i) == 'B') {
-                arr[charToInt(rings.charAt(i + 1))][2]++;
+    public String sortString(String s) {
+        int[] arr = new int[26];
+        for (int i = 0;i < s.length();i++) {
+            arr[s.charAt(i) - 97]++;
+        }
+
+        StringBuilder sf = new StringBuilder();
+        while (sf.length() != s.length()) {
+            for (int i = 0;i < arr.length;i++) {
+                if (arr[i] > 0) {
+                    sf.append((char) (97 + i));
+                    arr[i]--;
+                }
+            }
+            for (int i = arr.length - 1;i >= 0;i--) {
+                if (arr[i] > 0) {
+                    sf.append((char) (97 + i));
+                    arr[i]--;
+                }
             }
         }
 
-        for (int i = 0;i < arr.length;i++) {
-            if (arr[i][0] > 0 && arr[i][1] > 0 && arr[i][2] > 0) {
-                result++;
-            }
-        }
-
-        return result;
-    }
-
-    public int charToInt(char a) {
-        return Integer.parseInt(String.valueOf(a));
+        return sf.toString();
     }
 }
