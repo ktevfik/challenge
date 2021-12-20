c0c48efc660dc12de1c846818dafc4da3685a01c
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-12-20 23:30:24 +0300

    day 32 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,19 @@
-import java.util.Arrays;
-
 class Solution {
-    public String sortString(String s) {
-        int[] arr = new int[26];
-        for (int i = 0;i < s.length();i++) {
-            arr[s.charAt(i) - 97]++;
+    public String firstPalindrome(String[] words) {
+        for (int i = 0;i < words.length;i++) {
+            if (isPalindromic(words[i])) return words[i];
         }
 
-        StringBuilder sf = new StringBuilder();
-        while (sf.length() != s.length()) {
-            for (int i = 0;i < arr.length;i++) {
-                if (arr[i] > 0) {
-                    sf.append((char) (97 + i));
-                    arr[i]--;
-                }
-            }
-            for (int i = arr.length - 1;i >= 0;i--) {
-                if (arr[i] > 0) {
-                    sf.append((char) (97 + i));
-                    arr[i]--;
-                }
+        return "";
+    }
+
+    public boolean isPalindromic(String str) {
+        for (int i = 0,j = str.length() -1;i < j;i++,j--) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
             }
         }
 
-        return sf.toString();
+        return true;
     }
 }
