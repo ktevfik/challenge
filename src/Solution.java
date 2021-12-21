1093ac0ff463c40a30ef924fc1957ce76248cb66
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-12-21 20:27:15 +0300

    day 33 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,5 @@
 class Solution {
-    public String firstPalindrome(String[] words) {
-        for (int i = 0;i < words.length;i++) {
-            if (isPalindromic(words[i])) return words[i];
-        }
-
-        return "";
-    }
-
-    public boolean isPalindromic(String str) {
-        for (int i = 0,j = str.length() -1;i < j;i++,j--) {
-            if (str.charAt(i) != str.charAt(j)) {
-                return false;
-            }
-        }
-
-        return true;
+    public int minPartitions(String n) {
+        return n.chars().max().getAsInt() - '0';
     }
 }
