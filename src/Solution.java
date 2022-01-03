c6a88f84efb11f15472f6a2c9e2c6a7c65842283
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2022-01-03 23:11:42 +0300

    day 43 'ok'

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,31 @@
 class Solution {
-    public int mostWordsFound(String[] sentences) {
-        int result = 0;
-        for (int i = 0;i < sentences.length;i++) {
-            String[] strs = sentences[i].split(" ");
+    private int balls = 0;
+    private int steps = 0;
 
-            if (strs.length > result) result = strs.length;
+    public int[] minOperations(String boxes) {
+        int[] leftCount = new int[boxes.length()];
+        int[] rightCount = new int[boxes.length()];
+        int[] hopCount = new int[boxes.length()];
+
+        for(int i = 0; i < leftCount.length; i++) {
+            ballCounter(boxes.charAt(i), i, leftCount);
         }
-        return result;
+
+        steps = balls = 0;
+        for(int i = rightCount.length - 1; i > -1; i--) {
+            ballCounter(boxes.charAt(i), i, rightCount);
+        }
+
+        for(int i = 0; i < hopCount.length; i++) {
+            hopCount[i] = leftCount[i] + rightCount[i];
+        }
+
+        return hopCount;
+    }
+
+    private void ballCounter(char hasBall, int index, int[]counter) {
+        steps += balls;
+        balls += hasBall == '1' ? 1 : 0;
+        counter[index] = steps;
     }
 }
