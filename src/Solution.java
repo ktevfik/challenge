8ee93688ec938c0f3f7409c12bcb29696c4f8728
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2022-01-08 23:26:57 +0300

    day 46

--- src/Solution.java
+++ src/Solution.java
@@ -1,59 +1,17 @@
 class Solution {
-    private int balls = 0;
-    private int steps = 0;
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
 
-    public int[] minOperations(String boxes) {
-        int[] leftCount = new int[boxes.length()];
-        int[] rightCount = new int[boxes.length()];
-        int[] hopCount = new int[boxes.length()];
-
-        for(int i = 0; i < leftCount.length; i++) {
-            ballCounter(boxes.charAt(i), i, leftCount);
+        List<List<Integer>> res = new ArrayList();
+        Map<Integer, List<Integer>> groups = new HashMap<>();
+        for (int i = 0; i < groupSizes.length; ++i) {
+            List<Integer> list = groups.computeIfAbsent(groupSizes[i], k -> new ArrayList());
+            list.add(i);
+            if (list.size() == groupSizes[i]) {
+                res.add(list);
+                groups.put(groupSizes[i], new ArrayList());
+            }
         }
+        return res;
 
-        steps = balls = 0;
-        for(int i = rightCount.length - 1; i > -1; i--) {
-            ballCounter(boxes.charAt(i), i, rightCount);
-        }
-
-        for(int i = 0; i < hopCount.length; i++) {
-            hopCount[i] = leftCount[i] + rightCount[i];
-        }
-
-        return hopCount;
-    }
-
-    private void ballCounter(char hasBall, int index, int[]counter) {
-        steps += balls;
-        balls += hasBall == '1' ? 1 : 0;
-        counter[index] = steps;
     }
 }
-
-class OrderedStream {
-    int ptr;
-    String[] res;
-    
-    public OrderedStream(int n) {
-        ptr = 0;
-        res = new String[n];
-    }
-    
-    public List<String> insert(int id, String value) {
-        List<String> list = new ArrayList<>();
-        
-        res[id - 1] = value;
-        while (ptr < res.length && res[ptr] != null) {
-            list.add(res[ptr]);
-            ptr++;
-        }
-        
-        return list;
-    }
-}
-
-/**
- * Your OrderedStream object will be instantiated and called as such:
- * OrderedStream obj = new OrderedStream(n);
- * List<String> param_1 = obj.insert(idKey,value);
- */
